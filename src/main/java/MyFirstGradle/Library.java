3c84354a4427b49c6b9e7a025e2d5f29cc98edd9
ivan <ivan@ivan-Lenovo-Legion-Y530-15ICH>
2021-08-10 13:43:52 +0300

    Test .gitignore

--- src/main/java/MyFirstGradle/Library.java
+++ src/main/java/MyFirstGradle/Library.java
@@ -3,10 +3,35 @@
  */
 package MyFirstGradle;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Library {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		Scanner sc = new Scanner(System.in);
+
+		while (true) {
+			System.out.println("Please entr your name:");
+			String name = sc.next();
+			StringBuilder reverse = new StringBuilder(name);
+			reverse.reverse();
+
+			try {
+				FileWriter myWriter = new FileWriter("Myname.txt");
+				myWriter.write("Hello, " + reverse);
+				myWriter.close();
+				System.out.println("Successfully wrote to the file.");
+			} catch (IOException e) {
+				System.out.println("An error occurred.");
+				e.printStackTrace();
+			}
+
+			System.out.print("Welcome to home: " + reverse);
+			System.out.print("\n");
+		}
+
 	}
 	
     public boolean someLibraryMethod() {
