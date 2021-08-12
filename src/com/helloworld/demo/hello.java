36e8fed4c87bb12dcd34ec645de7ad57d9f0bbae
ivan <ivan@ivan-Lenovo-Legion-Y530-15ICH>
2021-08-12 11:20:40 +0300

    Using loop "for"

---
+++ src/com/helloworld/demo/hello.java
@@ -0,0 +1,13 @@
+package com.helloworld.demo;
+
+public class hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		for (int i = 0; i < 2; i++)
+		{
+			System.out.println("Hello");
+		}
+		
+	}
+}
